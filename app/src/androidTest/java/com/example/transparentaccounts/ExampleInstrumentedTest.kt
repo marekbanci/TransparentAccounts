2bbfb8d3738c7af264b3080efe4add8616cfaac8
marekbanci <marek.banci@csretail.cz>
2025-05-28 19:59:55 +0200

    Inspect repair

--- app/src/androidTest/java/com/example/transparentaccounts/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/transparentaccounts/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.transparentaccounts", appContext.packageName)
+        assertEquals("com.example.transparent accounts", appContext.packageName)
     }
 }
